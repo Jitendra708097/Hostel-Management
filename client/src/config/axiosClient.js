dba116c44a727405df74e276294d0eb049eae3ba
Jitendra Sharma <jitendravishvakarma708097@gmail.com>
2025-12-04 09:32:04 +0530

    updated

--- client/src/config/axiosClient.js
+++ client/src/config/axiosClient.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
+const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '';
 
 // this is a function which creates an axios 
 // instance with predefined configuration settings
 //  such as base URL and headers.
 const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
+  baseURL: isLocalhost ? 'http://localhost:3000' : "http://13.233.230.164:3000",
   withCredentials:true,
   headers: {
     'Content-Type': 'application/json',
